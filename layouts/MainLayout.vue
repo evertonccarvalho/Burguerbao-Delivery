ddabfa76cd062e87c6b2e536299647f69dd49302
Éverton Carvalho <evertonsnake@gmail.com>
2024-01-18 10:30:28 -0300

    header refactored

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -1,46 +1,98 @@
 <template>
 	<div id="MainLayout" class="w-full fixed z-50">
-		<div id="TopMenu" class="w-full bg-[#FAFAFA] border-b md:block hidden">
-			<ul
-				class="flex items-center justify-end text-xs text-[#333333] font-light px-2 h-10 bg-[#FAFAFA] max-w-[1200px]"
+		<div id="MainHeader" class="flex items-center w-full bg-black">
+			<div
+				class="flex lg:justify-start items-center justify-between gap-10 max-w-[1150px] w-full px-3 py-5 mx-auto"
 			>
-				<li
-					class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer"
-				>
-					Sell on BurguerBão
-				</li>
-				<li
-					class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer"
-				>
-					Cookie Preferences
-				</li>
-				<li
-					class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer"
-				>
-					Help
-				</li>
-				<li
-					class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer"
-				>
-					Buyer Protection
-				</li>
-				<li class="px-3 hover:text-[#FF4646] cursor-pointer">
-					<Icon name="ic:sharp-install-mobile" size="17" />
-					App
-				</li>
-				<li
+				<!-- Logo -->
+				<NuxtLink to="/" class="w-10">
+					<img src="/BurguerBao-logo.svg" />
+				</NuxtLink>
+				<!-- Logo -->
+
+				<!-- SearchinInput -->
+				<div class="w-full md:block">
+					<div class="relative">
+						<div
+							class="flex items-center p-0 m-0 border-none bg-white rounded-full w-full"
+						>
+							<input
+								class="w-full h-full bg-transparent text-black placeholder-gray-400 text-sm pl-3 focus:outline-none"
+								placeholder="kitchen accessories"
+								type="text"
+								v-model="searchItem"
+							/>
+							<Icon
+								v-if="isSearching"
+								name="eos-icons:loading"
+								size="25"
+								class="mr-2"
+							/>
+							<button
+								class="flex items-center w-14 h-8 p-1.5 px-4 m-1 rounded-full bg-black"
+							>
+								<Icon name="ph:magnifying-glass" size="20" color="#ffffff" />
+							</button>
+						</div>
+
+						<div
+							v-if="items && items.data"
+							class="absolute bg-white max-w-[700px] rounded-3xl p-4 mt-0.5 h-auto w-full"
+						>
+							<div v-for="item in items.data" :key="item.id" class="p-1">
+								<NuxtLink
+									:to="`/item/${item.id}`"
+									class="flex items-center justify-between w-full cursor-pointer hover:bg-gray-100"
+								>
+									<div class="flex items-center">
+										<img class="rounded-md" width="40" :src="item.url" />
+										<div class="truncate ml-2">{{ item.title }}</div>
+									</div>
+									<div class="truncate">R${{ item.price / 100 }}</div>
+								</NuxtLink>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- SearchinInput -->
+
+				<!-- ShopingCart -->
+				<NuxtLink to="/shoppingcart" class="flex items-center">
+					<button
+						class="relative"
+						@mouseenter="isCartHover = true"
+						@mouseleave="isCartHover = false"
+					>
+						<span
+							class="absolute flex items-center justify-center -right-[3px] top-0 bg-white h-[17px] min-w-[17px] text-xs text-black px-0.5 rounded-full"
+						>
+							{{ userStore.cart.length }}
+						</span>
+						<div class="min-w-[40px]">
+							<Icon
+								name="ph:shopping-cart-simple-light"
+								size="33"
+								:color="isCartHover ? '#ffff' : '#ffff'"
+							/>
+						</div>
+					</button>
+				</NuxtLink>
+				<!-- ShopingCart -->
+
+				<!-- ACCOUNT MENU -->
+				<div
 					@mouseenter="isAccountMenu = true"
 					@mouseleave="isAccountMenu = false"
-					class="relative flex items-center px-2.5 hover:text-[#FF4646] h-full cursor-pointer"
+					class="relative text-white gap-2 flex items-center hover:text-white h-full cursor-pointer"
 					:class="
 						isAccountMenu
-							? 'bg-white border z-40 shadow-[0_15px_100px_40px_rgba(0,0,0,0.3)]'
-							: 'border border-[#FAFAFA]'
+							? 'bg-transparent border-none z-40 shadow-[0_15px_100px_40px_rgba(0,0,0,0.3)]'
+							: 'border border-none'
 					"
 				>
-					<Icon name="ph:user-thin" size="17" />
+					<Icon name="ph:user-thin" size="33" />
 					Account
-					<Icon name="mdi:chevron-down" size="15" class="ml-5" />
+					<Icon name="mdi:chevron-down" size="15" />
 
 					<div
 						id="AccountMenu"
@@ -49,7 +101,7 @@
 					>
 						<div v-if="!user">
 							<div class="text-semibold text-[15px] my-4 px-3">
-								Welcome to BurguerBão!
+								Welcome to AliExpress!
 							</div>
 							<div class="flex items-center gap-1 px-3 mb-3">
 								<NuxtLink
@@ -77,90 +129,48 @@
 							</li>
 						</ul>
 					</div>
-				</li>
-			</ul>
-		</div>
-		<div id="MainHeader" class="flex items-center w-full bg-white">
-			<div
-				class="flex lg:justify-start justify-between gap-10 max-w-[1150px] w-full px-3 py-5 mx-auto"
-			>
-				<NuxtLink to="/" class="w-10">
-					<img src="/BurguerBao-logo.svg" />
-				</NuxtLink>
-
-				<div class="max-w-[700px] w-full md:block hidden">
-					<div class="relative">
-						<div
-							class="flex items-center border-2 border-[#FF4646] rounded-md w-full"
-						>
-							<input
-								class="w-full placeholder-gray-400 text-sm pl-3 focus:outline-none"
-								placeholder="kitchen accessories"
-								type="text"
-								v-model="searchItem"
-							/>
-							<Icon
-								v-if="isSearching"
-								name="eos-icons:loading"
-								size="25"
-								class="mr-2"
-							/>
-							<button
-								class="flex items-center h-[100%] p-1.5 px-2 bg-[#FF4646]"
-							>
-								<Icon name="ph:magnifying-glass" size="20" color="#ffffff" />
-							</button>
-						</div>
-
-						<div
-							v-if="items && items.data"
-							class="absolute bg-white max-w-[700px] h-auto w-full"
-						>
-							<div v-for="item in items.data" :key="item.id" class="p-1">
-								<NuxtLink
-									:to="`/item/${item.id}`"
-									class="flex items-center justify-between w-full cursor-pointer hover:bg-gray-100"
-								>
-									<div class="flex items-center">
-										<img class="rounded-md" width="40" :src="item.url" />
-										<div class="truncate ml-2">{{ item.title }}</div>
-									</div>
-									<div class="truncate">R${{ item.price / 100 }}</div>
-								</NuxtLink>
-							</div>
-						</div>
-					</div>
 				</div>
-
-				<NuxtLink to="/shoppingcart" class="flex items-center">
-					<button
-						class="relative md:block hidden"
-						@mouseenter="isCartHover = true"
-						@mouseleave="isCartHover = false"
-					>
-						<span
-							class="absolute flex items-center justify-center -right-[3px] top-0 bg-[#FF4646] h-[17px] min-w-[17px] text-xs text-white px-0.5 rounded-full"
-						>
-							{{ userStore.cart.length }}
-						</span>
-						<div class="min-w-[40px]">
-							<Icon
-								name="ph:shopping-cart-simple-light"
-								size="33"
-								:color="isCartHover ? '#FF4646' : ''"
-							/>
-						</div>
-					</button>
-				</NuxtLink>
-
+				<!-- ACCOUNT MENU -->
+				<!-- MOBILE MENU -->
 				<button
 					@click="userStore.isMenuOverlay = true"
-					class="md:hidden block rounded-full p-1.5 -mt-[4px] hover:bg-gray-200"
+					class="md:hidden block rounded-full p-1.5 -mt-[4px] text-white hover:bg-white hover:text-black"
 				>
 					<Icon name="radix-icons:hamburger-menu" size="33" />
 				</button>
+				<!-- MOBILE MENU -->
 			</div>
 		</div>
+		<!-- <div id="TopMenu" class="w-full bg-black border-b md:block hidden">
+			<ul
+				class="flex items-center justify-end text-sm font-normal text-white font-light px-2 h-10 bg-black max-w-[1200px]"
+			>
+				<li
+					class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer"
+				>
+					Sell on BurguerBão
+				</li>
+				<li
+					class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer"
+				>
+					Cookie Preferences
+				</li>
+				<li
+					class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer"
+				>
+					Help
+				</li>
+				<li
+					class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer"
+				>
+					Buyer Protection
+				</li>
+				<li class="px-3 hover:text-[#FF4646] cursor-pointer">
+					<Icon name="ic:sharp-install-mobile" size="17" />
+					App
+				</li>
+			</ul>
+		</div> -->
 	</div>
 
 	<Loading v-if="userStore.isLoading" />
