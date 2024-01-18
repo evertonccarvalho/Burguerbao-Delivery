11f4f1170ce623440c0851536857b0cf8cf4766c
Éverton Carvalho <evertonsnake@gmail.com>
2024-01-18 14:07:14 -0300

    category

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,124 +2,176 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-async function seedProducts() {
+async function main() {
 	try {
-		await prisma.products.create({
+		const hamburguerCategory = await prisma.category.create({
 			data: {
+				name: 'Hamburguer',
+				slug: 'hamburguer',
+				imageUrl: 'https://fsw-store.s3.sa-east-1.amazonaws.com/mouses.png',
+			},
+		});
+
+		const hamburguers = [
+			{
 				title: 'Hamburguer Clássico de Carne',
 				description:
 					'Suculento hambúrguer de carne bovina coberto com alface fresca, tomate, cebola e nosso molho especial. Servido em um pão de gergelim.',
 				url: 'https://picsum.photos/id/7/800/800',
 				price: 2500,
+				categoryId: hamburguerCategory.id,
+				discountPercentage: 10, // 10% discount
 			},
-		});
-
-		await prisma.products.create({
-			data: {
+			{
 				title: 'Hamburguer Vegetariano Delicioso',
 				description:
 					'Um delicioso hambúrguer vegetariano feito com uma mistura de vegetais frescos e ervas. Coberto com abacate, alface e tomate. Servido em um pão integral.',
 				url: 'https://picsum.photos/id/20/800/800',
 				price: 1999,
+				categoryId: hamburguerCategory.id,
+				discountPercentage: 10, // 10% discount
 			},
-		});
-
-		await prisma.products.create({
-			data: {
+			{
+				title: 'Hamburguer Vegetariano Delicioso',
+				description:
+					'Um delicioso hambúrguer vegetariano feito com uma mistura de vegetais frescos e ervas. Coberto com abacate, alface e tomate. Servido em um pão integral.',
+				url: 'https://picsum.photos/id/20/800/800',
+				price: 1999,
+				categoryId: hamburguerCategory.id,
+				discountPercentage: 10, // 10% discount
+			},
+			{
 				title: 'Hamburguer de Frango Apimentado',
 				description:
 					'Peito de frango grelhado marinado em uma mistura picante de ervas e especiarias. Coberto com jalapeños, queijo pepper jack e maionese de chipotle.',
 				url: 'https://picsum.photos/id/21/800/800',
 				price: 9999,
+				categoryId: hamburguerCategory.id,
+				discountPercentage: 10, // 10% discount
 			},
-		});
-
-		await prisma.products.create({
-			data: {
+			{
 				title: 'Hamburguer de Peixe Fresco',
 				description:
 					'Filete de peixe fresco preparado com uma crosta crocante. Servido com alface, tomate e molho tártaro em um pão de batata-doce.',
 				url: 'https://picsum.photos/id/24/800/800',
 				price: 5999,
+				categoryId: hamburguerCategory.id,
+				discountPercentage: 10, // 10% discount
 			},
-		});
-
-		await prisma.products.create({
-			data: {
+			{
 				title: 'Hamburguer Duplo Bacon',
 				description:
 					'Dois hambúrgueres suculentos cobertos com queijo cheddar derretido e fatias crocantes de bacon. Servido em um pão de hambúrguer tradicional.',
 				url: 'https://picsum.photos/id/34/800/800',
 				price: 6589,
+				categoryId: hamburguerCategory.id,
+				discountPercentage: 10, // 10% discount
 			},
-		});
-
-		await prisma.products.create({
-			data: {
+			{
 				title: 'Hamburguer de Costela Defumada',
 				description:
 					'Saboroso hambúrguer de costela defumada, regado com molho barbecue caseiro. Com alface, cebola caramelizada e queijo defumado. Servido em um pão de brioche.',
 				url: 'https://picsum.photos/id/45/800/800',
 				price: 7899,
+				categoryId: hamburguerCategory.id,
+				discountPercentage: 10, // 10% discount
 			},
-		});
-
-		await prisma.products.create({
-			data: {
-				title: 'Hamburguer Vegano de Quinoa',
+			{
+				title: 'Hamburguer de Costela Defumada',
 				description:
-					'Hambúrguer vegano feito com quinoa, feijão-preto e uma mistura de temperos. Coberto com abacate, rúcula e molho vegano especial. Servido em um pão de grãos integrais.',
-				url: 'https://picsum.photos/id/50/800/800',
-				price: 1299,
+					'Saboroso hambúrguer de costela defumada, regado com molho barbecue caseiro. Com alface, cebola caramelizada e queijo defumado. Servido em um pão de brioche.',
+				url: 'https://picsum.photos/id/45/800/800',
+				price: 7899,
+				categoryId: hamburguerCategory.id,
+				discountPercentage: 10, // 10% discount
 			},
-		});
-
-		await prisma.products.create({
-			data: {
+			{
 				title: 'Hamburguer Picante de Jalapeño',
 				description:
 					'Hambúrguer com carne temperada e picante, coberto com queijo pepper jack derretido, jalapeños fatiados e molho de pimenta. Servido em um pão de hambúrguer de pimenta.',
 				url: 'https://picsum.photos/id/60/800/800',
 				price: 1450,
+				categoryId: hamburguerCategory.id,
+				discountPercentage: 10, // 10% discount
+			},
+			{
+				title: 'Hamburguer Picante de Jalapeño',
+				description:
+					'Hambúrguer com carne temperada e picante, coberto com queijo pepper jack derretido, jalapeños fatiados e molho de pimenta. Servido em um pão de hambúrguer de pimenta.',
+				url: 'https://picsum.photos/id/60/800/800',
+				price: 1450,
+				categoryId: hamburguerCategory.id,
+				discountPercentage: 10, // 10% discount
+			},
+			{
+				title: 'Hamburguer Picante de Jalapeño',
+				description:
+					'Hambúrguer com carne temperada e picante, coberto com queijo pepper jack derretido, jalapeños fatiados e molho de pimenta. Servido em um pão de hambúrguer de pimenta.',
+				url: 'https://picsum.photos/id/60/800/800',
+				price: 1450,
+				categoryId: hamburguerCategory.id,
+				discountPercentage: 10, // 10% discount
+			},
+		];
+
+		await prisma.products.createMany({
+			data: hamburguers,
+		});
+
+		const refrigerantesCategory = await prisma.category.create({
+			data: {
+				name: 'Refrigerantes',
+				slug: 'refrigerantes',
+				imageUrl: 'https://fsw-store.s3.sa-east-1.amazonaws.com/keyboards.png',
 			},
 		});
 
-		await prisma.products.create({
-			data: {
-				title: 'Hamburguer Supreme de Frango',
-				description:
-					'Supremo hambúrguer de frango grelhado, coberto com queijo suíço, cogumelos salteados e molho especial. Servido em um pão de batata-doce.',
-				url: 'https://picsum.photos/id/70/800/800',
-				price: 8999,
+		const drinks = [
+			{
+				title: 'Coca-Cola',
+				description: 'Refrigerante gaseificado à base de cola.',
+				url: 'https://picsum.photos/id/7/800/800',
+				price: 2500,
+				categoryId: refrigerantesCategory.id,
+				discountPercentage: 10, // 10% discount
 			},
-		});
+			{
+				title: 'Pepsi',
+				description: 'Refrescante refrigerante com sabor de cola.',
+				url: 'https://picsum.photos/id/17/800/800',
+				price: 2300,
+				categoryId: refrigerantesCategory.id,
+				discountPercentage: 5, // 5% discount
+			},
+			{
+				title: 'Sprite',
+				description: 'Refrigerante cítrico e não cola com sabor de limão-lima.',
+				url: 'https://picsum.photos/id/27/800/800',
+				price: 2200,
+				categoryId: refrigerantesCategory.id,
+				discountPercentage: 0,
+			},
+			// Adicione mais refrigerantes conforme necessário
+		];
 
-		await prisma.products.create({
-			data: {
-				title: 'Hamburguer de Cordeiro Mediterrâneo',
-				description:
-					'Delicioso hambúrguer de cordeiro temperado com ervas mediterrâneas. Coberto com tzatziki, tomate e pepino. Servido em um pão de pita.',
-				url: 'https://picsum.photos/id/80/800/800',
-				price: 10999,
-			},
-		});
-
-		await prisma.products.create({
-			data: {
-				title: 'Hamburguer Clássico com Ovo',
-				description:
-					'Hambúrguer clássico de carne com ovo frito, bacon crocante e queijo americano. Servido em um pão de hambúrguer tradicional.',
-				url: 'https://picsum.photos/id/90/800/800',
-				price: 8799,
-			},
+		await prisma.products.createMany({
+			data: drinks,
 		});
 
 		// Adicione mais produtos conforme necessário
+		console.log('Seed completed successfully');
 	} catch (error) {
-		console.error(error);
+		console.error('Error seeding database:', error);
 	} finally {
 		await prisma.$disconnect();
 	}
 }
-
-seedProducts();
+main()
+	.then(async () => {
+		await prisma.$disconnect();
+	})
+	.catch(async (e) => {
+		console.error(e);
+		await prisma.$disconnect();
+		process.exit(1);
+	});
